1c0d04d4358babc035f28ab9f45b1de8d81707f0
Tore Berven <tore.berven@student.uib.no>
2021-02-11 21:34:17 +0100

    Created a class for reading input from text file.

--- src/main/java/assignment2/ReadFromFile.java
+++ src/main/java/assignment2/ReadFromFile.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class ReadFromFile {
+package assignment2;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFromFile {
+
+    public List<String> readFile(String filePath) {
+        List<String> lines = new ArrayList<>();
+        try {
+            lines = Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            System.out.println("Could not read the file: " + filePath);
+        }
+        return lines;
+    }
 }
