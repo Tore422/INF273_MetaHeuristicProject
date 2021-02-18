8a01190a2621f39cac4489623924b1754e5d2db3
Tore Berven <tore.berven@student.uib.no>
2021-02-18 17:27:25 +0100

    Removed old code and comments; made createRandomSolution() public

--- src/main/java/pickup/and/delivery/BlindRandomSearch.java
+++ src/main/java/pickup/and/delivery/BlindRandomSearch.java
@@ -25,85 +25,31 @@ public class BlindRandomSearch {
         return bestSolution;
     }
 
-    private static IVectorSolutionRepresentation<Integer> createRandomSolution(IVectorSolutionRepresentation<Integer> exampleSolution) {
+    /**
+     * Create a valid, random solution representation based on
+     * a given example solution.
+     *
+     * @param exampleSolution
+     * @return A valid permutation of the given solution.
+     */
+    public static IVectorSolutionRepresentation<Integer> createRandomSolution(IVectorSolutionRepresentation<Integer> exampleSolution) {
         List<Integer> randomSolution = new ArrayList<>(exampleSolution.getSolutionRepresentation());
         Collections.shuffle(randomSolution);
-        while (true) {
+        do {
             tryToCreateARandomValidSolution(randomSolution);
-            if (PickupAndDelivery.isValidSolution(new VectorSolutionRepresentation<>(randomSolution))) {
-                break;
-            }
-        }
+        } while (!PickupAndDelivery.isValidSolution(new VectorSolutionRepresentation<>(randomSolution)));
         return new VectorSolutionRepresentation<>(randomSolution);
-
-
-
-
-        /*
-        List<Integer> randomSolution = new ArrayList<>(exampleSolution.getSolutionSize());
-        List<Integer> elements = new ArrayList<>(exampleSolution.getSolutionRepresentation());
-        int indexOfPreviousZero = -1;
-        while (!elements.isEmpty()) {
-            int randomIndexNumber = (int) (elements.size() * Math.random());
-            Integer element = elements.get(randomIndexNumber);
-            elements.remove(randomIndexNumber);
-           // System.out.println("element = " + element);
-            if (element == 0) {
-                List<Integer> unfinishedCalls = new ArrayList<>();
-           //     System.out.println("got zero");
-           //     System.out.println("randomSolution = " + randomSolution);
-                for (int i = indexOfPreviousZero + 1; i < randomSolution.size(); i++) {
-                    Integer callId = randomSolution.get(i);
-                    if (unfinishedCalls.contains(callId)) {
-                        unfinishedCalls.remove(callId);
-                    } else {
-                        unfinishedCalls.add(callId);
-                    }
-                }
-                Collections.shuffle(unfinishedCalls);
-            //    System.out.println("unfinishedCalls = " + unfinishedCalls);
-                for (Integer callId : unfinishedCalls) {
-                    randomSolution.add(callId);
-                    elements.remove(callId);
-                }
-                randomSolution.add(element);
-            //    System.out.println("randomSolution = " + randomSolution);
-                indexOfPreviousZero = randomSolution.size() - 1;
-            } else {
-                randomSolution.add(element);
-            }
-        }
-       // System.out.println("randomSolution = " + randomSolution);
-        return new VectorSolutionRepresentation<>(randomSolution);*/
-
-
-      /*  List<Integer> randomSolutionCandidate = new ArrayList<>(exampleSolution.solutionSize());
-        List<Integer> elements = new ArrayList<>(exampleSolution.getSolutionRepresentation());
-        Collections.shuffle(elements);
-        createRandomSolutionCandidate(randomSolutionCandidate, elements);
-        makeSolutionCandidateValid(randomSolutionCandidate, elements);
-        return new VectorSolutionRepresentation<>(randomSolutionCandidate);*/
     }
 
     private static void tryToCreateARandomValidSolution(List<Integer> randomSolution) {
         List<Integer> elements = new ArrayList<>(randomSolution.size() / 3);
-        List<Integer> unfinishedCalls = new ArrayList<>(randomSolution.size() / 3);
-        findUnfinishedCalls(randomSolution, elements, unfinishedCalls);
+        findUnfinishedCalls(randomSolution, elements);
         randomSolution.removeAll(elements);
         randomlyAddAllUnfinishedCalls(randomSolution, elements);
     }
 
-    private static void randomlyAddAllUnfinishedCalls(List<Integer> randomSolution, List<Integer> elements) {
-        Collections.shuffle(elements);
-        while (!elements.isEmpty()) {
-            Integer element = elements.get(elements.size() - 1);
-            elements.remove(elements.size() - 1);
-            int randomIndexNumber = (int) (randomSolution.size() * Math.random());
-            randomSolution.add(randomIndexNumber, element);
-        }
-    }
-
-    private static void findUnfinishedCalls(List<Integer> randomSolution, List<Integer> elements, List<Integer> unfinishedCalls) {
+    private static void findUnfinishedCalls(List<Integer> randomSolution, List<Integer> elements) {
+        List<Integer> unfinishedCalls = new ArrayList<>(randomSolution.size() / 3);
         for (Integer element : randomSolution) {
             if (element == 0) {
                 elements.addAll(unfinishedCalls);
@@ -119,38 +65,13 @@ public class BlindRandomSearch {
         elements.addAll(unfinishedCalls); // Add the unfinished calls from those outsourced
     }
 
-    private static void makeSolutionCandidateValid(List<Integer> randomSolutionCandidate, List<Integer> elements) {
-        List<Integer> unfinishedCalls = getUnfinishedCallsFromSolutionCandidate(randomSolutionCandidate, elements);
-        elements.addAll(unfinishedCalls);
-        randomSolutionCandidate.removeAll(elements); // First remove illegal calls from the solution,
-        randomSolutionCandidate.addAll(elements); // then add them as outsourced calls.
-    }
-
-    private static void createRandomSolutionCandidate(List<Integer> randomSolutionCandidate, List<Integer> elements) {
+    private static void randomlyAddAllUnfinishedCalls(List<Integer> randomSolution, List<Integer> elements) {
+        Collections.shuffle(elements);
         while (!elements.isEmpty()) {
-            int randomIndexNumber = (int) (elements.size() * Math.random());
-            Integer element = elements.get(randomIndexNumber);
-            randomSolutionCandidate.add(element);
-            elements.remove(randomIndexNumber);
+            Integer element = elements.get(elements.size() - 1);
+            elements.remove(elements.size() - 1);
+            int randomIndexNumber = (int) (randomSolution.size() * Math.random());
+            randomSolution.add(randomIndexNumber, element);
         }
-    }
-
-    private static List<Integer> getUnfinishedCallsFromSolutionCandidate(List<Integer> randomSolutionCandidate, List<Integer> elements) {
-        List<Integer> unfinishedCalls = new ArrayList<>(randomSolutionCandidate.size());
-        for (Integer element : randomSolutionCandidate) {
-            if (element == 0) {
-                if (!unfinishedCalls.isEmpty()) {
-                    elements.addAll(unfinishedCalls);
-                    unfinishedCalls.clear();
-                }
-            } else {
-                if (unfinishedCalls.contains(element)) {
-                    unfinishedCalls.remove(element);
-                } else {
-                    unfinishedCalls.add(element);
-                }
-            }
-        }
-        return unfinishedCalls;
     }
 }
