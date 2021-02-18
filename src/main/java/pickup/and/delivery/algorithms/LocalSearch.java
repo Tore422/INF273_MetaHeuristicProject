31004bcfc8982ebdadad62ccd559a0255a5fdcdc
Tore Berven <tore.berven@student.uib.no>
2021-02-19 00:59:36 +0100

    Finished setting up the algorithms for Local and Simulated annealing.
    Now only requires operators to be implemented,
    and for settings to be fine tuned.

--- src/main/java/pickup/and/delivery/algorithms/LocalSearch.java
+++ src/main/java/pickup/and/delivery/algorithms/LocalSearch.java
@@ -1,19 +1,36 @@
 package pickup.and.delivery.algorithms;
 
+import pickup.and.delivery.PickupAndDelivery;
+import pickup.and.delivery.operators.OneReinsert;
+import pickup.and.delivery.operators.ThreeExchange;
+import pickup.and.delivery.operators.TwoExchange;
 import solution.representations.vector.IVectorSolutionRepresentation;
-import solution.representations.vector.VectorSolutionRepresentation;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class LocalSearch {
 
     public static IVectorSolutionRepresentation<Integer> localSearch(IVectorSolutionRepresentation<Integer> initialSolution) {
-        List<Integer> solution = new ArrayList<>(initialSolution.getSolutionRepresentation());
-
-
-        return new VectorSolutionRepresentation<>(solution);
+        double probabilityOfUsingTwoExchange = 0.33;
+        double probabilityOfUsingThreeExchange = 0.33;
+        double probabilityOfUsingOneReinsert = 1 - probabilityOfUsingTwoExchange - probabilityOfUsingThreeExchange;
+        IVectorSolutionRepresentation<Integer> bestSolution = initialSolution;
+        IVectorSolutionRepresentation<Integer> currentSolution;
+        int NUMBER_OF_ITERATIONS = 10000;
+        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+            int operatorChoice = (int) (Math.random());
+            System.out.println("operatorChoice = " + operatorChoice);
+            if (operatorChoice < probabilityOfUsingTwoExchange) {
+                currentSolution = TwoExchange.useTwoExchangeOnSolution(bestSolution);
+            } else if (operatorChoice < (probabilityOfUsingTwoExchange + probabilityOfUsingThreeExchange)) {
+                currentSolution = ThreeExchange.useThreeExchangeOnSolution(bestSolution);
+            } else {
+                currentSolution = OneReinsert.useOneReinsertOnSolution(bestSolution);
+            }
+            if (PickupAndDelivery.feasible(currentSolution)
+                    && (PickupAndDelivery.calculateCost(currentSolution)
+                    < PickupAndDelivery.calculateCost(bestSolution))) {
+                bestSolution = currentSolution;
+            }
+        }
+        return bestSolution;
     }
-
-
 }
--- src/main/java/pickup/and/delivery/algorithms/SimulatedAnnealing.java
+++ src/main/java/pickup/and/delivery/algorithms/SimulatedAnnealing.java
@@ -1,19 +1,49 @@
 package pickup.and.delivery.algorithms;
 
+import pickup.and.delivery.PickupAndDelivery;
+import pickup.and.delivery.operators.OneReinsert;
+import pickup.and.delivery.operators.ThreeExchange;
+import pickup.and.delivery.operators.TwoExchange;
 import solution.representations.vector.IVectorSolutionRepresentation;
-import solution.representations.vector.VectorSolutionRepresentation;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class SimulatedAnnealing {
 
     public static IVectorSolutionRepresentation<Integer> simulatedAnnealingSearch(IVectorSolutionRepresentation<Integer> initialSolution) {
-        List<Integer> solution = new ArrayList<>(initialSolution.getSolutionRepresentation());
-
-
-        return new VectorSolutionRepresentation<>(solution);
+        double probabilityOfUsingTwoExchange = 0.33;
+        double probabilityOfUsingThreeExchange = 0.33;
+        double probabilityOfUsingOneReinsert = 1 - probabilityOfUsingTwoExchange - probabilityOfUsingThreeExchange;
+        double initialTemperature = 100;
+        double coolingFactor = 0.1;
+        double temperature = initialTemperature;
+        IVectorSolutionRepresentation<Integer> bestSolution = initialSolution;
+        IVectorSolutionRepresentation<Integer> currentlyAcceptedSolution = initialSolution;
+        IVectorSolutionRepresentation<Integer> newSolution = null;
+        final int NUMBER_OF_ITERATIONS = 10000;
+        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+            double operatorChoice = (Math.random());
+            System.out.println("operatorChoice = " + operatorChoice);
+            if (operatorChoice < probabilityOfUsingTwoExchange) {
+                newSolution = TwoExchange.useTwoExchangeOnSolution(currentlyAcceptedSolution);
+            } else if (operatorChoice < (probabilityOfUsingTwoExchange + probabilityOfUsingThreeExchange)) {
+                newSolution = ThreeExchange.useThreeExchangeOnSolution(currentlyAcceptedSolution);
+            } else {
+                newSolution = OneReinsert.useOneReinsertOnSolution(currentlyAcceptedSolution);
+            }
+            double deltaE = PickupAndDelivery.calculateCost(newSolution)
+                    - PickupAndDelivery.calculateCost(currentlyAcceptedSolution);
+            boolean newSolutionIsFeasible = PickupAndDelivery.feasible(newSolution);
+            if (newSolutionIsFeasible && deltaE < 0) {
+                currentlyAcceptedSolution = newSolution;
+                if (PickupAndDelivery.calculateCost(currentlyAcceptedSolution)
+                        < PickupAndDelivery.calculateCost(bestSolution)) {
+                    bestSolution = currentlyAcceptedSolution;
+                }
+            } else if (newSolutionIsFeasible
+                    && (Math.random() < Math.exp(-deltaE / temperature))) {
+                currentlyAcceptedSolution = newSolution;
+            }
+            temperature = coolingFactor * temperature;
+        }
+        return bestSolution;
     }
-
-
 }
