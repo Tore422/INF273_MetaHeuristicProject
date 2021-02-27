f0c2388e2dda925821ddf3bd602e78ef21ef1cf6
Tore Berven <tore.berven@student.uib.no>
2021-02-27 20:34:01 +0100

    Replaced Math.random() with RANDOM

--- src/main/java/pickup/and/delivery/algorithms/SimulatedAnnealing.java
+++ src/main/java/pickup/and/delivery/algorithms/SimulatedAnnealing.java
@@ -6,7 +6,11 @@ import pickup.and.delivery.operators.ThreeExchange;
 import pickup.and.delivery.operators.TwoExchange;
 import solution.representations.vector.IVectorSolutionRepresentation;
 
+import java.util.Random;
+
 public class SimulatedAnnealing {
+
+    private static final Random RANDOM = new Random();
 
     public static IVectorSolutionRepresentation<Integer> simulatedAnnealingSearch(IVectorSolutionRepresentation<Integer> initialSolution) {
         double probabilityOfUsingTwoExchange = 0.33;
@@ -20,15 +24,10 @@ public class SimulatedAnnealing {
         IVectorSolutionRepresentation<Integer> newSolution;
         final int NUMBER_OF_ITERATIONS = 10000;
         for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
-            double operatorChoice = (Math.random());
-            System.out.println("operatorChoice = " + operatorChoice);
-            if (operatorChoice < probabilityOfUsingTwoExchange) {
-                newSolution = TwoExchange.useTwoExchangeOnSolution(currentlyAcceptedSolution);
-            } else if (operatorChoice < (probabilityOfUsingTwoExchange + probabilityOfUsingThreeExchange)) {
-                newSolution = ThreeExchange.useThreeExchangeOnSolution(currentlyAcceptedSolution);
-            } else {
-                newSolution = OneReinsert.useOneReinsertOnSolution(currentlyAcceptedSolution);
-            }
+            double operatorChoice = RANDOM.nextDouble();
+            newSolution = applySelectedOperatorOnSolution(
+                    probabilityOfUsingTwoExchange, probabilityOfUsingThreeExchange,
+                    currentlyAcceptedSolution, operatorChoice);
             double deltaE = PickupAndDelivery.calculateCost(newSolution)
                     - PickupAndDelivery.calculateCost(currentlyAcceptedSolution);
             boolean newSolutionIsFeasible = PickupAndDelivery.feasible(newSolution);
@@ -36,14 +35,27 @@ public class SimulatedAnnealing {
                 currentlyAcceptedSolution = newSolution;
                 if (PickupAndDelivery.calculateCost(currentlyAcceptedSolution)
                         < PickupAndDelivery.calculateCost(bestSolution)) {
+        //            System.out.println("operatorChoice = " + operatorChoice);
                     bestSolution = currentlyAcceptedSolution;
                 }
             } else if (newSolutionIsFeasible
-                    && (Math.random() < Math.exp(-deltaE / temperature))) {
+                    && (RANDOM.nextDouble() < Math.exp(-deltaE / temperature))) {
                 currentlyAcceptedSolution = newSolution;
             }
             temperature = coolingFactor * temperature;
         }
         return bestSolution;
+    }
+
+    private static IVectorSolutionRepresentation<Integer> applySelectedOperatorOnSolution(double probabilityOfUsingTwoExchange, double probabilityOfUsingThreeExchange, IVectorSolutionRepresentation<Integer> currentlyAcceptedSolution, double operatorChoice) {
+        IVectorSolutionRepresentation<Integer> newSolution;
+        if (operatorChoice < probabilityOfUsingTwoExchange) {
+            newSolution = TwoExchange.useTwoExchangeOnSolution(currentlyAcceptedSolution);
+        } else if (operatorChoice < (probabilityOfUsingTwoExchange + probabilityOfUsingThreeExchange)) {
+            newSolution = ThreeExchange.useThreeExchangeOnSolution(currentlyAcceptedSolution);
+        } else {
+            newSolution = OneReinsert.useOneReinsertOnSolution(currentlyAcceptedSolution);
+        }
+        return newSolution;
     }
 }
