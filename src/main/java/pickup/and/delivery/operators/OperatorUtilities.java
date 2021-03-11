c14f6c7bb069ab714b4919a1d4bb5090ac00c563
Tore Berven <tore.berven@student.uib.no>
2021-03-11 19:52:23 +0100

    Made methods computing cost for vehicle, and finding vehicle number

--- src/main/java/pickup/and/delivery/operators/OperatorUtilities.java
+++ src/main/java/pickup/and/delivery/operators/OperatorUtilities.java
@@ -1,12 +1,15 @@
 package pickup.and.delivery.operators;
 
-import pickup.and.delivery.PickupAndDelivery;
+import pickup.and.delivery.entities.Call;
+import pickup.and.delivery.entities.NodeTimesAndCosts;
 import pickup.and.delivery.entities.Vehicle;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+
+import static pickup.and.delivery.PickupAndDelivery.*;
 
 public class OperatorUtilities {
 
@@ -16,7 +19,7 @@ public class OperatorUtilities {
     public static void findStartIndicesOfVehiclesThatCanTakeTheCall(
             List<Integer> zeroIndices, Integer firstPartOfCallToReinsert,
             List<Integer> startIndexOfVehiclesThatCanTakeTheCall) {
-        for (Vehicle vehicle : PickupAndDelivery.getVehicles()) {
+        for (Vehicle vehicle : getVehicles()) {
             for (Integer possibleCall : vehicle.getPossibleCalls()) {
                 if (possibleCall.equals(firstPartOfCallToReinsert)) {
                     if (vehicle.getIndex() == 1) {
@@ -134,7 +137,14 @@ public class OperatorUtilities {
         return zeroIndices;
     }
 
-
+    /**
+     * Returns a list with start and stop indices of vehicles
+     * handling two or more calls.<br>
+     * Does not include vehicle for outsourced calls.
+     *
+     * @param zeroIndices
+     * @return
+     */
     public static List<int[]> findVehiclesWithMoreThanOneCall(List<Integer> zeroIndices) {
         List<int[]> startAndStopIndicesOfVehiclesWithMoreThanOneCall = new ArrayList<>(zeroIndices.size());
         System.out.println("zeroIndices = " + zeroIndices);
@@ -160,4 +170,63 @@ public class OperatorUtilities {
         }
         return startAndStopIndicesOfVehiclesWithMoreThanOneCall;
     }
+
+    /* Assumes valid solution */
+    public static int computeCostForVehicle(int startIndex, int stopIndex, int vehicleNumber,
+                                     List<Integer> solutionRepresentation) {
+        int totalCost = 0;
+        int currentNode = getVehicles().get(vehicleNumber - 1).getHomeNode();
+        List<Call> unfinishedCalls = new ArrayList<>();
+        for (int i = startIndex + 1; i < stopIndex; i++) {
+            System.out.println("element = " + solutionRepresentation.get(i));
+            Call currentCall = getCalls().get(solutionRepresentation.get(i));
+            NodeTimesAndCosts nodeTimesAndCosts = getNodeTimesAndCostsForVehicle(
+                    vehicleNumber, currentCall.getCallIndex());
+            int destinationNode = - 1;
+            if (unfinishedCalls.contains(currentCall)) {
+                destinationNode = currentCall.getDestinationNode();
+                totalCost += nodeTimesAndCosts.getDestinationNodeCosts();
+                unfinishedCalls.remove(currentCall);
+            } else {
+                destinationNode = currentCall.getOriginNode();
+                totalCost += nodeTimesAndCosts.getOriginNodeCosts();
+                unfinishedCalls.add(currentCall);
+            }
+            totalCost += getTravelCostForJourney(currentNode, destinationNode, vehicleNumber);
+            currentNode = destinationNode;
+        }
+        return totalCost;
+    }
+
+/*  public static boolean computeIfTimeWindowHasBeenViolated() {
+
+        return false;
+    }
+*/
+
+/*  public static boolean computeIfCapacityHasBeenExceeded() {
+
+    }
+*/
+
+    public static int findVehicleNumberForVehicleStartingAtIndex(int startIndex, List<Integer> zeroIndices) {
+        int vehicleNumber = 1;
+        if (startIndex == -1) {
+            return vehicleNumber;
+        }
+        for (int i = 0; i < zeroIndices.size() - 1; i++) {
+            vehicleNumber++;
+            int zeroIndex = zeroIndices.get(i);
+            if (zeroIndex == startIndex) {
+                return vehicleNumber;
+            }
+        }
+        if (zeroIndices.get(zeroIndices.size() - 1).equals(startIndex)) {
+            throw new IllegalArgumentException("Start index matches vehicle for outsourced calls");
+        } else {
+            throw new IllegalArgumentException("Given start index does not match for any vehicle");
+        }
+    }
+
+
 }
