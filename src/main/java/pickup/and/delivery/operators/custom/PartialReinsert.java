1b6396953ab627c172c97ffc53966027cbfe8048
Tore Berven <tore.berven@student.uib.no>
2021-03-13 14:54:33 +0100

    Refactoring and removed debugging lines. Works, but results disappoint

--- src/main/java/pickup/and/delivery/operators/custom/PartialReinsert.java
+++ src/main/java/pickup/and/delivery/operators/custom/PartialReinsert.java
@@ -16,6 +16,9 @@ public class PartialReinsert {
     public static void main(String[] args) {
       //  List<Integer> values = Arrays.asList(3, 3, 0, 7, 1, 7, 1, 0, 5, 5, 6, 6, 0, 4, 2, 4, 2);
         List<Integer> values = Arrays.asList(96, 21, 21, 41, 96, 1, 1, 41, 0, 84, 92, 84, 92, 81, 53, 81, 53, 0, 5, 29, 8, 8, 5, 29, 0, 103, 103, 31, 109, 109, 31, 0, 26, 26, 14, 24, 24, 14, 0, 114, 6, 6, 114, 75, 75, 0, 58, 58, 60, 42, 60, 42, 113, 113, 0, 118, 118, 93, 93, 0, 82, 82, 2, 2, 0, 34, 34, 0, 97, 97, 119, 119, 0, 91, 91, 59, 59, 100, 100, 0, 52, 52, 10, 10, 0, 18, 18, 22, 22, 19, 36, 36, 19, 0, 61, 61, 98, 20, 98, 20, 0, 39, 39, 51, 51, 0, 94, 94, 55, 40, 55, 40, 0, 74, 28, 28, 74, 4, 4, 0, 65, 65, 62, 62, 130, 130, 0, 89, 89, 67, 78, 67, 78, 0, 43, 43, 112, 112, 15, 15, 0, 120, 122, 120, 122, 27, 27, 129, 124, 124, 129, 0, 80, 66, 66, 80, 99, 99, 0, 73, 73, 105, 105, 0, 77, 77, 87, 87, 125, 125, 0, 70, 70, 0, 17, 83, 17, 83, 11, 11, 95, 95, 46, 46, 0, 107, 107, 127, 127, 0, 32, 32, 110, 110, 0, 116, 116, 0, 69, 69, 12, 12, 85, 85, 0, 102, 126, 128, 126, 102, 128, 0, 90, 90, 54, 54, 23, 23, 0, 50, 111, 50, 111, 0, 115, 115, 13, 106, 13, 106, 0, 121, 121, 44, 44, 3, 3, 0, 108, 117, 108, 117, 45, 45, 0, 123, 123, 0, 7, 37, 57, 7, 37, 57, 88, 88, 0, 49, 35, 38, 35, 49, 38, 0, 25, 47, 101, 30, 25, 76, 56, 48, 101, 79, 16, 79, 76, 86, 86, 64, 63, 56, 47, 63, 33, 72, 71, 64, 71, 9, 72, 68, 33, 30, 9, 68, 48, 16, 104, 104);
+        System.out.println("values = " + values);
+        List<Integer> a = new ArrayList<>(values.subList(0, 9));
+        System.out.println("values = " + a);
 
         IVectorSolutionRepresentation<Integer> sol = new VectorSolutionRepresentation<>(values);
         System.out.println("feasible(sol) = " + feasible(sol));
@@ -33,78 +36,30 @@ public class PartialReinsert {
             return randomlyReinsertPartOfAnOutsourcedCall(newSolution, zeroIndices);
         }
         List<Integer> vehiclesProcessedSoFar = new ArrayList<>(indicesOfVehiclesToProcess.size());
-
-       // while (true) {
+        while (true) {
             int randomIndex = RANDOM.nextInt(indicesOfVehiclesToProcess.size());
             if (!vehiclesProcessedSoFar.contains(randomIndex)) {
-                vehiclesProcessedSoFar.add(randomIndex);
-
                 // Process vehicle
                 int[] startAndStopIndexOfVehicle = indicesOfVehiclesToProcess.get(randomIndex);
-                System.out.println("startAndStopIndexOfVehicle = " + Arrays.toString(startAndStopIndexOfVehicle));
                 int vehicleNumber = findVehicleNumberForVehicleStartingAtIndex(
                         startAndStopIndexOfVehicle[0], zeroIndices);
-                System.out.println("vehicleNumber = " + vehicleNumber);
                 int costOfInitialSolution = computeCostForVehicle(
                         startAndStopIndexOfVehicle[0], startAndStopIndexOfVehicle[1],
                         vehicleNumber, newSolutionRepresentation);
-                System.out.println("costOfInitialSolution = " + costOfInitialSolution);
-                int numberOfDifferentCallsInVehicle = findNumberOfDifferentCallsInVehicle(
-                        startAndStopIndexOfVehicle[0], startAndStopIndexOfVehicle[1]);
-                System.out.println("numberOfDifferentCallsInVehicle = " + numberOfDifferentCallsInVehicle);
-                int bestCostSoFarForCurrentVehicle = costOfInitialSolution;
-                int indexOfCallToMove = -1, indexToInsertCall = -1;
-                int startIndex = startAndStopIndexOfVehicle[0];
-                int stopIndex = startAndStopIndexOfVehicle[1];
-                for (int i = startIndex + 1; i < stopIndex; i++) {
-                    List<Integer> copyOfSolutionRepresentation = new ArrayList<>(newSolutionRepresentation);
-                    int currentCall = copyOfSolutionRepresentation.remove(i);
-                    for (int j = startIndex + 1; j < stopIndex; j++) {
-                        copyOfSolutionRepresentation.add(j, currentCall);
-                        boolean timeWindowConstraintHolds = timeWindowConstraintHoldsFor(
-                                startIndex, stopIndex, vehicleNumber, copyOfSolutionRepresentation);
-                        boolean vehicleCapacityConstraintHolds = vehicleCapacityConstraintHoldsFor(
-                                startIndex, stopIndex, vehicleNumber, copyOfSolutionRepresentation);
-                        if (timeWindowConstraintHolds && vehicleCapacityConstraintHolds) {
-                            int costOfSolutionCandidate = computeCostForVehicle(
-                                    startIndex, stopIndex, vehicleNumber, copyOfSolutionRepresentation);
-                            if (costOfSolutionCandidate < bestCostSoFarForCurrentVehicle) {
-                                indexOfCallToMove = i;
-                                indexToInsertCall = j;
-                                bestCostSoFarForCurrentVehicle = costOfSolutionCandidate;
-                            }
-                        }
-                        copyOfSolutionRepresentation.remove(j);
-                    }
-                }
-                System.out.println("costOfInitialSolution = " + costOfInitialSolution);
-                System.out.println("bestCostSoFarForCurrentVehicle = " + bestCostSoFarForCurrentVehicle);
-                if (bestCostSoFarForCurrentVehicle > costOfInitialSolution) {
-                    int partOfCallToReinsert = newSolutionRepresentation.get(indexOfCallToMove);
-                    newSolutionRepresentation.add(indexToInsertCall, partOfCallToReinsert);
+                boolean foundImprovement = processVehicleLookingForImprovement(
+                        newSolution, startAndStopIndexOfVehicle, vehicleNumber, costOfInitialSolution);
+                if (foundImprovement) {
                     return newSolution;
                 }
-
-
-
-
-                // otherwise we try a different vehicle.
-
+                vehiclesProcessedSoFar.add(randomIndex);
             }
             if (vehiclesProcessedSoFar.size() >= indicesOfVehiclesToProcess.size()) {
                 //Tried all possibilities without finding an improvement,
-                //so we select random element from outsourced calls,
+                //so we select a random element from outsourced calls,
                 //and insert that element into a random position there.
-
-
-                return newSolution;
+                return randomlyReinsertPartOfAnOutsourcedCall(newSolution, zeroIndices);
             }
-       // }
-    return newSolution;
-
-
-
-
+        }
 
 /*
     from solution
@@ -114,7 +69,6 @@ public class PartialReinsert {
     and add start index to list.
 
     Pick a random vehicle, not outsourced, from that list.
-    Calculate capacity, travel time and cost
 
     Calculate cost of current setup.
     For each index within that vehicle,
@@ -123,15 +77,12 @@ public class PartialReinsert {
     If capacity and time limitations are upheld,
     and the new cost is lower than previous best,
     set this index as the best found position so far.
+    Repeat for each index (n^2 runtime)
 
-    When finished, insert the partial call into the best found position,
-    if no improvement was found, try selecting a different random index
-    (blacklist previously tested indices)
-    and repeat the process within the vehicle.
-
-    If no improvement is possible, try selecting a different vehicle,
+    When finished, insert the partial call into the best found position.
+    If no improvement was found, try selecting a different vehicle,
     (blacklisting previously tested vehicles)
-    and repeat the process.
+    and repeat the process within the new vehicle.
 
     If still no improvement is possible,
     randomly select a partial call within outsourced
@@ -143,6 +94,45 @@ public class PartialReinsert {
 */
     }
 
+    private static boolean processVehicleLookingForImprovement(
+            IVectorSolutionRepresentation<Integer> newSolution, int[] startAndStopIndexOfVehicle,
+            int vehicleNumber, int costOfInitialSolution) {
+        List<Integer> newSolutionRepresentation = newSolution.getSolutionRepresentation();
+        int bestCostSoFarForCurrentVehicle = costOfInitialSolution;
+        int indexOfCallToMove = -1, indexToInsertCall = -1;
+        int startIndex = startAndStopIndexOfVehicle[0];
+        int stopIndex = startAndStopIndexOfVehicle[1];
+        for (int i = startIndex + 1; i < stopIndex; i++) {
+            List<Integer> copyOfSolutionRepresentation = new ArrayList<>(
+                    newSolutionRepresentation.subList(0, stopIndex + 1));
+            int currentCall = copyOfSolutionRepresentation.remove(i);
+            for (int j = startIndex + 1; j < stopIndex; j++) {
+                copyOfSolutionRepresentation.add(j, currentCall);
+                boolean timeWindowConstraintHolds = timeWindowConstraintHoldsFor(
+                        startIndex, stopIndex, vehicleNumber, copyOfSolutionRepresentation);
+                boolean vehicleCapacityConstraintHolds = vehicleCapacityConstraintHoldsFor(
+                        startIndex, stopIndex, vehicleNumber, copyOfSolutionRepresentation);
+                if (timeWindowConstraintHolds && vehicleCapacityConstraintHolds) {
+                    int costOfSolutionCandidate = computeCostForVehicle(
+                            startIndex, stopIndex, vehicleNumber, copyOfSolutionRepresentation);
+                    if (costOfSolutionCandidate < bestCostSoFarForCurrentVehicle) {
+                        indexOfCallToMove = i;
+                        indexToInsertCall = j;
+                        bestCostSoFarForCurrentVehicle = costOfSolutionCandidate;
+                    }
+                }
+                copyOfSolutionRepresentation.remove(j);
+            }
+        }
+        if (bestCostSoFarForCurrentVehicle < costOfInitialSolution) {
+          //  System.out.println("Improved solution by: " + (costOfInitialSolution - bestCostSoFarForCurrentVehicle));
+            int partOfCallToReinsert = newSolutionRepresentation.remove(indexOfCallToMove);
+            newSolutionRepresentation.add(indexToInsertCall, partOfCallToReinsert);
+            return true;
+        }
+        return false;
+    }
+
     private static IVectorSolutionRepresentation<Integer> randomlyReinsertPartOfAnOutsourcedCall(
             IVectorSolutionRepresentation<Integer> newSolution, List<Integer> zeroIndices) {
         // Select random element from outsourced calls,
@@ -151,13 +141,9 @@ public class PartialReinsert {
         int startIndex = zeroIndices.get(zeroIndices.size() - 1);
         int stopIndex = newSolutionRepresentation.size();
         int randomIndex = findRandomIndexWithinVehicle(startIndex, stopIndex, null);
-        System.out.println("randomIndex = " + randomIndex);
         int randomlySelectedCall = newSolutionRepresentation.remove(randomIndex);
-        System.out.println("randomlySelectedCall = " + randomlySelectedCall);
         randomIndex = findRandomIndexWithinVehicle(startIndex, stopIndex - 1, null);
-        System.out.println("newRandomIndex = " + randomIndex);
         newSolutionRepresentation.add(randomIndex, randomlySelectedCall);
-        System.out.println("newSolutionRepresentation = " + newSolutionRepresentation);
         return newSolution;
     }
 }
