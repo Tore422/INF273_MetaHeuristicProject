cde6466de8f7b16e4531bea29a9d8c99b33afd4a
Tore Berven <tore.berven@student.uib.no>
2021-04-14 14:03:14 +0200

    Added functionality for operator usage statistics, and weights work
    Added methods for printing all operator statistics,
    and for printing operator statistics from specific iterations.
    
    Added tracking of the number of times each operator is used
    for each segment.
    
    Segment based data is reset after updating weights.
    
    Weights are now updated and normalized.
    
    Added 0 as identifier for escape algorithm operator.

--- src/main/java/pickup/and/delivery/algorithms/GeneralAdaptiveMetaheuristic.java
+++ src/main/java/pickup/and/delivery/algorithms/GeneralAdaptiveMetaheuristic.java
@@ -15,22 +15,18 @@ import static pickup.and.delivery.operators.OperatorUtilities.RANDOM;
 public class GeneralAdaptiveMetaheuristic {
 
     public static void main(String[] args) {
-        List<Integer> values = Arrays.asList(0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7);
+        //List<Integer> values = Arrays.asList(0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7);
+        List<Integer> values = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 23, 24, 24, 25, 25, 26, 26, 27, 27, 28, 28, 29, 29, 30, 30, 31, 31, 32, 32, 33, 33, 34, 34, 35, 35, 36, 36, 37, 37, 38, 38, 39, 39, 40, 40, 41, 41, 42, 42, 43, 43, 44, 44, 45, 45, 46, 46, 47, 47, 48, 48, 49, 49, 50, 50, 51, 51, 52, 52, 53, 53, 54, 54, 55, 55, 56, 56, 57, 57, 58, 58, 59, 59, 60, 60, 61, 61, 62, 62, 63, 63, 64, 64, 65, 65, 66, 66, 67, 67, 68, 68, 69, 69, 70, 70, 71, 71, 72, 72, 73, 73, 74, 74, 75, 75, 76, 76, 77, 77, 78, 78, 79, 79, 80, 80, 81, 81, 82, 82, 83, 83, 84, 84, 85, 85, 86, 86, 87, 87, 88, 88, 89, 89, 90, 90, 91, 91, 92, 92, 93, 93, 94, 94, 95, 95, 96, 96, 97, 97, 98, 98, 99, 99, 100, 100, 101, 101, 102, 102, 103, 103, 104, 104, 105, 105, 106, 106, 107, 107, 108, 108, 109, 109, 110, 110, 111, 111, 112, 112, 113, 113, 114, 114, 115, 115, 116, 116, 117, 117, 118, 118, 119, 119, 120, 120, 121, 121, 122, 122, 123, 123, 124, 124, 125, 125, 126, 126, 127, 127, 128, 128, 129, 129, 130, 130);
+
         IVectorSolutionRepresentation<Integer> sol = new VectorSolutionRepresentation<>(values);
         IVectorSolutionRepresentation<Integer> bestSolution = adaptiveMetaheuristicSearch(sol);
         System.out.println("bestSolution = " + bestSolution);
         System.out.println("best cost = " + PickupAndDelivery.calculateCost(bestSolution));
-
-        System.out.println("Operator performance stats");
-        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
-            System.out.println();
-            System.out.println("iteration = " + i);
-            int operator = operatorSelected.get(i);
-            System.out.println("operator = " + Operators.getOperatorNameFromID(operator));
-            System.out.println("objectiveValuesBeforeOperatorUse = " + objectiveValuesBeforeOperatorUse.get(i));
-            System.out.println("objectiveValuesAfterOperatorUse = " + objectiveValuesAfterOperatorUse.get(i));
-            System.out.println("newSolutionWasFeasible = " + newSolutionWasFeasible.get(i));
-        }
+        //printOperatorUsageStatistics();
+        printOperatorUsageStatisticsForIteration(0);
+        printOperatorUsageStatisticsForIteration(100);
+        printOperatorUsageStatisticsForIteration(1000);
+        printOperatorUsageStatisticsForIteration(9999);
     }
 
     enum Operators {
@@ -66,6 +62,7 @@ public class GeneralAdaptiveMetaheuristic {
     private static final int THRESHOLD_FOR_ESCAPING_LOCAL_OPTIMA = 500;
     private static Map<Integer, Double> operatorWeights;
     private static Map<Integer, Integer> scores;
+    private static Map<Integer, Integer> numberOfTimesEachOperatorHasBeenUsedThisSegment;
     private static int selectedOperator;
     private static boolean foundNewBestSolutionThisIteration;
 
@@ -84,6 +81,7 @@ public class GeneralAdaptiveMetaheuristic {
         System.out.println("INITIAL_OPERATOR_WEIGHTS = " + INITIAL_OPERATOR_WEIGHTS);
         operatorWeights = initializeOperatorWeights();
         scores = initializeScores();
+        numberOfTimesEachOperatorHasBeenUsedThisSegment = initializeOperatorUsageCounter();
 
         System.out.println("Operators.getOperators() = " + Operators.getOperatorsWithID());
 
@@ -107,29 +105,30 @@ public class GeneralAdaptiveMetaheuristic {
                 for (int j = 0; j < NUMBER_OF_ESCAPE_ITERATIONS; j++) {
                     currentSolution = useEscapeAlgorithmOnSolution(currentSolution);
                     System.out.println("currentSolution = " + currentSolution);
-                    int objectiveCostForCurrentSolution = PickupAndDelivery.calculateCost(currentSolution);
-                    if (objectiveCostForCurrentSolution < bestObjectiveFoundSoFar) {
+                    int objectiveCostOfNewCurrentSolution = PickupAndDelivery.calculateCost(currentSolution);
+                    registerOperatorUsageStatistics(objectiveCostOfCurrentSolution, objectiveCostOfNewCurrentSolution,
+                            PickupAndDelivery.feasible(currentSolution));
+                    objectiveCostOfCurrentSolution = objectiveCostOfNewCurrentSolution;
+                    if (objectiveCostOfNewCurrentSolution < bestObjectiveFoundSoFar) {
                         bestSolution = currentSolution;
-                        bestObjectiveFoundSoFar = objectiveCostForCurrentSolution;
+                        bestObjectiveFoundSoFar = objectiveCostOfNewCurrentSolution;
                         break; // Found new global best, so we have definitely left the local optima
                     }
                 }
                 numberOfIterationsSincePreviousBestWasFound = 0; // Reset to avoid escaping again immediately
             } else {
-                objectiveValuesBeforeOperatorUse.add(objectiveCostOfCurrentSolution);
                 IVectorSolutionRepresentation<Integer> newSolution = selectAndApplyOperatorOnSolution(currentSolution);
                 int objectiveCostOfNewSolution = PickupAndDelivery.calculateCost(newSolution);
-                objectiveValuesAfterOperatorUse.add(objectiveCostOfNewSolution);
-                newSolutionWasFeasible.add(PickupAndDelivery.feasible(newSolution));
-                operatorSelected.add(selectedOperator);
+                boolean newSolutionIsFeasible = PickupAndDelivery.feasible(newSolution);
+                registerOperatorUsageStatistics(objectiveCostOfCurrentSolution,
+                        objectiveCostOfNewSolution, newSolutionIsFeasible);
                 System.out.println("selectedOperator = " + selectedOperator);
                 System.out.println("newSolution = " + newSolution);
-                if (!PickupAndDelivery.feasible(newSolution)) {
+                if (!newSolutionIsFeasible) {
                     System.out.println("Error, not a feasible solution");
                 }
-
                 if (objectiveCostOfNewSolution < bestObjectiveFoundSoFar
-                        && PickupAndDelivery.feasible(newSolution)) { // Should always be feasible?
+                        && newSolutionIsFeasible) { // Should always be feasible?
                     System.out.println("Found a new best solution");
                     bestSolution = newSolution;
                     bestObjectiveFoundSoFar = objectiveCostOfNewSolution;
@@ -151,6 +150,55 @@ public class GeneralAdaptiveMetaheuristic {
             }
         }//*/
         return bestSolution;
+    }
+
+    private static void registerOperatorUsageStatistics(int objectiveCostOfCurrentSolution,
+                                                        int objectiveCostOfNewSolution, boolean newSolutionIsFeasible) {
+        objectiveValuesBeforeOperatorUse.add(objectiveCostOfCurrentSolution);
+        objectiveValuesAfterOperatorUse.add(objectiveCostOfNewSolution);
+        newSolutionWasFeasible.add(newSolutionIsFeasible);
+        operatorSelected.add(selectedOperator);
+    }
+
+    private static void printOperatorUsageStatistics() {
+        int numberOfTimesEscapeAlgorithmWasUsed = 0;
+        int lastIterationEscapeAlgorithmWasUsed = 0;
+        System.out.println("Operator performance stats");
+        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+            System.out.println();
+            System.out.println("iteration = " + i);
+            int operator = operatorSelected.get(i);
+            if (operator == 0) {
+                System.out.println("operator = Escape algorithm");
+                numberOfTimesEscapeAlgorithmWasUsed++;
+                lastIterationEscapeAlgorithmWasUsed = i;
+            } else {
+                System.out.println("operator = " + Operators.getOperatorNameFromID(operator));
+            }
+            System.out.println("objectiveValuesBeforeOperatorUse = " + objectiveValuesBeforeOperatorUse.get(i));
+            System.out.println("objectiveValuesAfterOperatorUse = " + objectiveValuesAfterOperatorUse.get(i));
+            System.out.println("newSolutionWasFeasible = " + newSolutionWasFeasible.get(i));
+
+            System.out.println("numberOfTimesEscapeAlgorithmWasUsed = "
+                    + numberOfTimesEscapeAlgorithmWasUsed / NUMBER_OF_ESCAPE_ITERATIONS);
+            System.out.println("lastIterationEscapeAlgorithmWasUsed = " + lastIterationEscapeAlgorithmWasUsed);
+        }
+    }
+
+    private static void printOperatorUsageStatisticsForIteration(int iterationNumber) {
+        System.out.println();
+        System.out.println("iteration = " + iterationNumber);
+        int operator = operatorSelected.get(iterationNumber);
+        if (operator == 0) {
+            System.out.println("operator = Escape algorithm");
+        } else {
+            System.out.println("operator = " + Operators.getOperatorNameFromID(operator));
+        }
+        System.out.println("objectiveValuesBeforeOperatorUse = "
+                + objectiveValuesBeforeOperatorUse.get(iterationNumber));
+        System.out.println("objectiveValuesAfterOperatorUse = "
+                + objectiveValuesAfterOperatorUse.get(iterationNumber));
+        System.out.println("newSolutionWasFeasible = " + newSolutionWasFeasible.get(iterationNumber));
     }
 
     private static final int ZERO = 0;
@@ -175,7 +223,17 @@ public class GeneralAdaptiveMetaheuristic {
         return operatorWeights;
     }
 
-    private static final int SEGMENT_SIZE = 10;
+    private static Map<Integer, Integer> initializeOperatorUsageCounter() {
+        Map<Integer, Integer> initialOperatorUsage = new HashMap<>();
+        for (int i = 1; i <= NUMBER_OF_OPERATORS; i++) {
+            initialOperatorUsage.put(i, ZERO);
+        }
+        System.out.println("initialOperatorUsage.values() = " + initialOperatorUsage.values());
+        System.out.println("initialOperatorUsage.keySet() = " + initialOperatorUsage.keySet());
+        return initialOperatorUsage;
+    }
+
+    private static final int SEGMENT_SIZE = 100;
 
     private static void updateOperatorSelectionParameters(
             IVectorSolutionRepresentation<Integer> newSolution,
@@ -190,23 +248,39 @@ public class GeneralAdaptiveMetaheuristic {
     }
 
     private static final double MINIMUM_WEIGHT = 0.05;
-    private static final double R = INITIAL_OPERATOR_WEIGHTS; // What value should this have?
-
+    private static final double R = 0.2;// Reaction factor [0,1].
+                                                             // Determines how quickly the weights should change.
     private static void updateWeights(int iterationNumber) {
         double sum = 0;
-        for (int i = 1; i <= NUMBER_OF_OPERATORS; i++) {
-            double oldWeight = operatorWeights.get(i);
-            int score = scores.get(i);
-            double theta = (double) SEGMENT_SIZE;
+        for (int currentOperatorID = 1; currentOperatorID <= NUMBER_OF_OPERATORS; currentOperatorID++) {
+            double oldWeight = operatorWeights.get(currentOperatorID);
+            double score = (double) scores.get(currentOperatorID);
+            double theta = Math.max(1.0, numberOfTimesEachOperatorHasBeenUsedThisSegment.get(currentOperatorID));
             double newWeight = oldWeight * (1.0 - R) + (R * (score / theta));
-            operatorWeights.put(i, Math.max(newWeight, MINIMUM_WEIGHT));
+            operatorWeights.put(currentOperatorID, Math.max(newWeight, MINIMUM_WEIGHT));
             System.out.println("theta = " + theta);
             System.out.println("oldWeight = " + oldWeight);
-            System.out.println("newWeight = " + newWeight);
-            sum += operatorWeights.get(i);
+            sum += operatorWeights.get(currentOperatorID);
         }
-        System.out.println("sum = " + sum);
-        // How to update weights?
+        for (int currentOperatorID = 1; currentOperatorID <= NUMBER_OF_OPERATORS; currentOperatorID++) {
+            double normalizedNewWeight = operatorWeights.get(currentOperatorID) / sum;
+            operatorWeights.put(currentOperatorID, normalizedNewWeight);
+            System.out.println("normalizedNewWeight = " + normalizedNewWeight);
+        }
+        resetScores();
+        resetOperatorUsagePerSegment();
+    }
+
+    private static void resetScores() {
+        for (int i = 1; i <= NUMBER_OF_OPERATORS; i++) {
+            scores.put(i, ZERO);
+        }
+    }
+
+    private static void resetOperatorUsagePerSegment() {
+        for (int i = 1; i <= NUMBER_OF_OPERATORS; i++) {
+            numberOfTimesEachOperatorHasBeenUsedThisSegment.put(i, ZERO);
+        }
     }
 
     private static final int SCORE_FOR_FINDING_UNEXPLORED_SOLUTION = 1;
@@ -258,6 +332,7 @@ public class GeneralAdaptiveMetaheuristic {
         return PickupAndDelivery.feasible(newSolution);
     }
 
+    private static final int ONE = 1;
 
     private static IVectorSolutionRepresentation<Integer> selectAndApplyOperatorOnSolution(
             IVectorSolutionRepresentation<Integer> currentSolution) {
@@ -276,12 +351,15 @@ public class GeneralAdaptiveMetaheuristic {
             newSolution = PartialReinsert.usePartialReinsertOnSolution(currentSolution);
             selectedOperator = operatorsWithID.get(Operators.PARTIAL_REINSERT);
         }
+        int oldNumberOfOperatorUsage = numberOfTimesEachOperatorHasBeenUsedThisSegment.get(selectedOperator);
+        numberOfTimesEachOperatorHasBeenUsedThisSegment.put(selectedOperator, oldNumberOfOperatorUsage + ONE);
         return newSolution;
     }
 
     // TODO: Replace with an operator that only provides divers, feasible solutions!
     private static IVectorSolutionRepresentation<Integer> useEscapeAlgorithmOnSolution(
             IVectorSolutionRepresentation<Integer> solution) {
+        selectedOperator = 0;
         IVectorSolutionRepresentation<Integer> newSolution;
         do {
             newSolution = ThreeExchange.useThreeExchangeOnSolution(solution);
